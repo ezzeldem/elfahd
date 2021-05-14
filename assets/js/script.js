4fd5ddc6e4bf409542b8d2a08d798454d9758f46
ezzeldem <ezzelden06@gmail.com>
2021-05-14 07:54:47 +0200

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,4 +103,19 @@ $(document).ready(function () {
     AOS.init({ duration: 600 });
 
 
+
+    import {getCLS, getFID, getLCP} from 'web-vitals';
+
+function sendToAnalytics(metric) {
+  const body = JSON.stringify(metric);
+  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+  (navigator.sendBeacon && navigator.sendBeacon('/analytics', body)) ||
+      fetch('/analytics', {body, method: 'POST', keepalive: true});
+}
+
+getCLS(sendToAnalytics);
+getFID(sendToAnalytics);
+getLCP(sendToAnalytics);
+
+
 });
